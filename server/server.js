8ca578d6523f6fe89cfbe1fc9c8547791e595c0b
betonit007 <timnagorski@hotmail.com>
2020-09-17 16:29:43 -0400

    added '()' to cors to intiate

--- server/server.js
+++ server/server.js
@@ -1,16 +1,14 @@
 import express from 'express'
 import { connectDB } from './db/db.js'
-import  chatRouter from './routes/chats.js'
+import chatRouter from './routes/chats.js'
 import cors from 'cors'
 
 const PORT = process.env.PORT || 5000
 const app = express()
 
-
-
 // middleware
 app.use(express.json({ extended: false })); // we can now accept info from req.body
-app.use(cors)
+app.use(cors())
 
 //connect to DB
 connectDB()
