01aa61cc8f148bb0774eaad7bc62cd3eaef56106
betonit007 <timnagorski@hotmail.com>
2020-09-17 16:12:51 -0400

    added cors

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import { connectDB } from './db/db.js'
 import  chatRouter from './routes/chats.js'
+import cors from 'cors'
 
 const PORT = process.env.PORT || 5000
 const app = express()
@@ -9,11 +10,7 @@ const app = express()
 
 // middleware
 app.use(express.json({ extended: false })); // we can now accept info from req.body
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Headers", "*")
-    next()
-})
+app.use(cors)
 
 //connect to DB
 connectDB()
