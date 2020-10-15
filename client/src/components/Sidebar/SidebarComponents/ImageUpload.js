4e281507f9a57a5b3a2c1c085d6a66f22eadbf51
betonit007 <timnagorski@hotmail.com>
2020-10-15 12:32:28 -0400

    back to old toggle menu

---
+++ client/src/components/Sidebar/SidebarComponents/ImageUpload.js
@@ -0,0 +1,100 @@
+import React, { useState } from 'react'
+import Resizer from 'react-image-file-resizer'
+import { LinearProgress } from '@material-ui/core/';
+import axios from '../../../axios'
+
+
+const ImageUpload = ({ user, token, updateUser, userMenu, toggleUserMenu, singleUpload = true }) => {
+    
+    const [ picLoading, setPicLoading ] = useState(false)
+    const [ images, setImages ] = useState(false)
+
+    const fileResizeAndUpload = async (event) => { //npm package react-image-file-resizer
+        setPicLoading(true)
+        let fileInput = false
+        if (event.target.files[0]) {
+            fileInput = true
+        }
+        if (fileInput) {
+            Resizer.imageFileResizer(
+                event.target.files[0],
+                1000,
+                1000,
+                'JPEG',
+                100,
+                0,
+                async uri => {
+                    try {
+                        const { data } = await axios.post('/api/auth/image', { image: uri }, {
+                            headers: {
+                                authtoken: token
+                            }
+                        })
+                        updateUser(data)
+
+                    } catch (error) {
+                        console.log('Error uploading photo(s)', error)
+                    } finally {
+                        setPicLoading(false)
+                        toggleUserMenu(!userMenu)
+                    }
+
+                },
+                'base64'
+            );
+        }
+    }
+
+    const deleteImage = async (id) => {
+
+        try {
+            setPicLoading(true)
+            await axios.delete('/api/auth/avatar', {
+                public_id: id
+            }, {
+                headers: {
+                    authtoken: user.token
+                }
+            })
+            if (singleUpload) {
+                setImages({ url: '', public_id: '' })
+
+            } else {
+                let filteredImages = images.filter(image => image.public_id !== id)
+                setImages(filteredImages)
+            }
+
+        } catch (error) {
+            console.log(error)
+        } finally {
+            setPicLoading(false)
+        }
+
+    }
+
+    return (
+       
+            <div className="image-upload" >
+                    <label style={{cursor:"pointer"}}>
+                        <div>
+                            {!picLoading ?
+                                <div style={{color: 'darkblue'}}>{user.photoUrl ? "Change" : "Select" } Avatar</div>
+                                :
+                               <LinearProgress />}
+                        </div>
+                        <input
+                            disabled={picLoading}
+                            hidden
+                            accept="image/*"
+                            placeholder='Images'
+                            type="file"
+                            onChange={fileResizeAndUpload}
+                        />
+                    </label>
+    
+            </div>
+        
+    )
+}
+
+export default ImageUpload
